a1d2b025c66158e834faf2fe12b52a2f7b48dee7
yvonnegichovi <yvonnegichovi@gmail.com>
2023-12-16 08:59:12 -0800

    fixing

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -21,10 +21,10 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
+				swapped = 1;
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swapped = 1;
 				print_array(array, size);
 			}
 		}
